41f5956f3ef7f111c4d4fabc48f5ef8e839f7fb9
Ashwin <ashwinkv.akv@gmail.com>
2023-09-17 12:38:53 +0530

    user login signup design done design of both signup and signin done backend connection left error message handling left

--- middlewares/userAuth.js
+++ middlewares/userAuth.js
@@ -2,6 +2,7 @@ module.exports=function userAuth(req, res, next) {
     if (req.session.user) {
         next(); // User is authenticated, continue to the next middleware or route
     } else {
-        res.render('login', { errorMessage: '' });
+        // res.render('signup')
+        res.render('login', { errorMessage: 'sdfawsedf' });
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./views/*.{ejs,html}"],
   theme: {
-    extend: {},
+    // colors: {
+    //   'brandColor': '#076AE1',
+    //   'white':'#FFFFFF'
+    // },
+    extend: {
+      fontFamily: {
+        'sans': ['Montserrat', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 
+                  '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 
+                  'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', 
+                  '"Noto Color Emoji"'],
+      }
+    },
   },
   plugins: [],
 }
