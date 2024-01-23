949fa720c95c2d5c81c6f9bdc2d5f32c1f286685
Ashwin <ashwinkv.akv@gmail.com>
2024-01-23 12:55:21 +0530

    url update

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 // Connect to the MongoDB database
 const url = process.env.DB_CONNECT
-console.log("db url"+url);
 module.exports = () => {
    return mongoose.connect(url);
 }
